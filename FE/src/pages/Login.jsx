7a67e8c47fd734a60f1ed50cf84440b9297b2b27
harithas1 <163287893+harithas1@users.noreply.github.com>
2025-01-29 12:45:02 +0530

    commit

--- FE/src/pages/Login.jsx
+++ FE/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import bookloginbg from "../assets/bookloginbg.avif";
 
 export default function Login({ setRole, setToken, setId}) {
   const [form, setForm] = useState({
