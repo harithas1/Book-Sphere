34795e0dec930dfb3539a099e1f2b975c5768ee3
harithas1 <163287893+harithas1@users.noreply.github.com>
2025-01-30 21:42:49 +0530

    commit

--- FE/src/pages/Admin.jsx
+++ FE/src/pages/Admin.jsx
@@ -337,7 +337,7 @@ export default function Admin({ token, role, id }) {
             <h2 className="text-2xl font-semibold mb-4">Customers Details</h2>
             <input
               // value={selectedUserName}
-              className="border border-gray-300 px-2 rounded-md"
+              className="border border-gray-300 p-2 rounded-md"
               onChange={(e) => setSelectedUserName(e.target.value)}
               type="text"
               placeholder="🔍 Search"
